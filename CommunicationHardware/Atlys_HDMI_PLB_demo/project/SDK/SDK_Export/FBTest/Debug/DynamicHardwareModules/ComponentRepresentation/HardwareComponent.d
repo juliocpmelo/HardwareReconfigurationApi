e577579dccd43ab718a8bc857d0992021fb44332
Julio Melo <julio.engcomp@gmail.com>
2013-05-31 19:29:18 +0200

    Removing previous version
    Replacing the Atlys example since it is not compatible with PR

--- CommunicationHardware/Atlys_HDMI_PLB_demo/project/SDK/SDK_Export/FBTest/Debug/DynamicHardwareModules/ComponentRepresentation/HardwareComponent.d
+++
@@ -1,8 +0,0 @@
-DynamicHardwareModules/ComponentRepresentation/HardwareComponent.d: \
- ../DynamicHardwareModules/ComponentRepresentation/HardwareComponent.c \
- ../DynamicHardwareModules/ComponentRepresentation/HardwareComponent.h \
- ../DynamicHardwareModules/ComponentRepresentation/../HardwareInterface.h
-
-../DynamicHardwareModules/ComponentRepresentation/HardwareComponent.h:
-
-../DynamicHardwareModules/ComponentRepresentation/../HardwareInterface.h:
--- CommunicationHardware/Atlys_HDMI_PLB_demo/project/SDK/SDK_Export/FBTest/Debug/DynamicHardwareModules/HardwareReconfigurationModule.d
+++
@@ -1,5 +0,0 @@
-DynamicHardwareModules/HardwareReconfigurationModule.d: \
- ../DynamicHardwareModules/HardwareReconfigurationModule.c \
- ../DynamicHardwareModules/HardwareReconfigurationModule.h
-
-../DynamicHardwareModules/HardwareReconfigurationModule.h:
