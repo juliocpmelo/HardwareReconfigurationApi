408abc11492e7f9ac497e0158e0d526d83b57256
Julio Melo <julio.engcomp@gmail.com>
2013-08-02 17:47:04 +0200

    Adding functional version of the CommunicationHardwareGenerator
    This version still doesn't recognize the parameters in a right way,
    still working into a way to handle them

--- communicationHardwareGenerator/XPSIpGenerator/XPSIpGenerator.cpp
+++ communicationHardwareGenerator/XPSIpGenerator/XPSIpGenerator.cpp
@@ -5,74 +5,118 @@ using namespace std;
 
 
 #include "HardwareComponentXmlParser.h"
+#include "HardwareComponentConverterVHDL.h"
+
+#include <systemc>
 
 
-class CommunicationHardwareXmlParser: public HardwareComponentParser
+class CommunicationHardwareXmlParser : public HardwareComponentXmlParser
 {
 	public:
 		/*variables*/
 	public:
 		/*methods*/
-		CommunicationHardwareParser();
+		CommunicationHardwareXmlParser();
 		/*
 		 * parses the xml description of the communication hardware entity
 		 * converting it into a HardwareComponent instance
 		 */
+
 		HardwareComponent* parseXml(std::string communicationHardwareXml);
 		HardwareComponent* parseCommunicationHardwareNode(xmlNode *communicationHardwareNode);
 
 };
 
-CommunicationHardwareXmlParse::CommunicationHardwareParser(){}
 
-HardwareComponent* CommunicationHardwareXmlParse::parseCommunicationHardwareNode(xmlNode *communicationHardwareNode){
+class CommunicationHardwareConverterVHDL : public HardwareComponentConverterVHDL
+{
+	public:
+		/*variables*/
+	public:
+		/*methods*/
+		CommunicationHardwareConverterVHDL();
+		/*
+		 * !brief generates a .vhl output containing a component description of the specific
+		 * communication hardware given as argument.
+		 * !param componentPath path to put the component declaration
+		 * !param communicationHardware instance of a communication hardware
+		 */
+		void buildComponentDescription(std::string componentPath, HardwareComponent* communicationHardware);
+};
+
+CommunicationHardwareXmlParser::CommunicationHardwareXmlParser(){}
+
+
+HardwareComponent* CommunicationHardwareXmlParser::parseCommunicationHardwareNode(xmlNode *communicationHardwareNode){
 	xmlNode *currentNode = NULL;
 
 	HardwareComponent::HardwareComponentInfo* componentInfo = new HardwareComponent::HardwareComponentInfo();
 	componentInfo->name = "CommunicationHardware";
 
-	HardwareComponent::PortInfo clkPort{"commHardware_clk", HardwareComponent::DataType_bit, 1};
 
 	for (currentNode = communicationHardwareNode->xmlChildrenNode; currentNode; currentNode = currentNode->next) {
 
+	cout<<__FILE__<<"::"<<__LINE__<<endl;
 		if(xmlStrcmp(currentNode->name, (const xmlChar *)"param") == 0){
 
 			/*test if the name is add or data and create the corresponding constraints*/
-			HardwareComponent::PortInfo info = parsePort(currentNode);
-			componentInfo->inputs[inputNameStr] = info;
+			HardwareComponent::ParamInfo info = parseParam(currentNode);
+			componentInfo->componentParameters[info.name] = info;
 
-			HardwareComponent::PortInfo clkPort{"commHardware_addr", HardwareComponent::DataType_bit, 1};
-			HardwareComponent::PortInfo clkPort{"commHardware_data", HardwareComponent::DataType_bit, 1};
 		}
 
 		else if(xmlStrcmp(currentNode->name, (const xmlChar *)"input") == 0){
 
 			HardwareComponent::PortInfo info = parsePort(currentNode);
-			componentInfo->outputs[outputNameStr] = info;
+			componentInfo->outputs[info.name] = info;
 		}
 		else if(xmlStrcmp(currentNode->name, (const xmlChar *)"output") == 0){
 
 			HardwareComponent::PortInfo info = parsePort(currentNode);
-			componentInfo->outputs[outputNameStr] = info;
+			componentInfo->outputs[info.name] = info;
 		}
 
 	}
 
-	return new HardwareComponent(sc_name("communicationHardware_inst"), componentInfo);
+	cout<<__FILE__<<"::"<<__LINE__<<endl;
+	/*define dataWidth and addressWidth if the user didnt*/
+	if (componentInfo->componentParameters.count("commHardware_dataWidth") == 0){
+		HardwareComponent::ParamInfo info = {"commHardware_dataWidth", HardwareComponent::DataType_integer, 32, "32"};
+		componentInfo->componentParameters[info.name] = info;	
+	}
+	cout<<__FILE__<<"::"<<__LINE__<<endl;
+	if (componentInfo->componentParameters.count("commHardware_addressWidth") == 0){
+		HardwareComponent::ParamInfo info = {"commHardware_addressWidth", HardwareComponent::DataType_integer, 32, "32"};
+		componentInfo->componentParameters[info.name] = info;
+	}
+
+	/*ovewrite possible user-defined inputs*/
+	HardwareComponent::PortInfo dataPort = {"commHardware_data", HardwareComponent::DataType_vector, 32};
+	componentInfo->inputs[dataPort.name] = dataPort;
+	HardwareComponent::PortInfo addressPort = {"commHardware_address", HardwareComponent::DataType_vector, 32};
+	componentInfo->inputs[addressPort.name] = addressPort;
+	
+	HardwareComponent::PortInfo clkPort = {"commHardware_clk", HardwareComponent::DataType_bit, 1};
+	componentInfo->inputs[clkPort.name] = clkPort;
+		
+
+	return new HardwareComponent(sc_module_name("communicationHardware_inst"), componentInfo);
 
 }
 
 HardwareComponent* CommunicationHardwareXmlParser::parseXml(std::string communicationHardwareXml){
 	
+	xmlDoc         *doc = NULL;
+	xmlNode        *root_element = NULL;
 	doc = xmlReadFile(communicationHardwareXml.c_str(), NULL, 0);
 
-	map<string, HardwareComponent::HardwareComponentInfo*> retMap;
+	map<string, HardwareComponent::HardwareComponentInfo*> retMapi;
 
 	HardwareComponent *component;
 
 	if (doc == NULL)
 	{
-		cout<<"error: could not parse file "<<xmlProjectFile<<endl;
+		cout<<"error: could not parse file "<<communicationHardwareXml<<endl;
 	}
 	else
 	{
@@ -94,27 +138,19 @@ HardwareComponent* CommunicationHardwareXmlParser::parseXml(std::string communic
 }
 
 
-int main(int argc, char** argv){
+CommunicationHardwareConverterVHDL::CommunicationHardwareConverterVHDL(){}
 
-	xmlDoc         *doc = NULL;
-	xmlNode        *root_element = NULL;
-	char workingDir[256];
-
+void CommunicationHardwareConverterVHDL::buildComponentDescription(std::string componentPath, HardwareComponent* communicationHardwareComponent){
+	
 	ofstream outFile;
-	string fileLocation = string(argv[2])+"/"+topComponent->name()+".vhdl";
-	cout<<"generating vhdl to component "<<topComponent->name()<<" file :"<<fileLocation<<endl;
+	string fileLocation = string(componentPath);
+	cout<<"generating vhdl to component "<<endl;
 	outFile.open (fileLocation.c_str());
 
-	
-
-	CommunicationHardwareXmlParser * parser = new CommunicationHardwareXmlParser();
-	HardwareComponent* communicationHardwareComponent = parser->parseXml(arg[1]);
-
-
-	outFile <<"component "<<communicationHardwareComponent->name()<<" is"<<endl; //TODO place the main tag name here
+	outFile <<"component "<<communicationHardwareComponent->componentInfo->name<<" is"<<endl; //TODO place the main tag name here
 	outFile <<"port ("<<endl;
 
-	std::map<std::string, HardwareComponent::PortInfo*> portsTable = topComponent->ports;
+	std::map<std::string, HardwareComponent::PortInfo*> portsTable = communicationHardwareComponent->ports;
 
 	map<string, HardwareComponent::PortInfo*>::iterator lastElement = portsTable.end();
 	lastElement --;
@@ -127,16 +163,32 @@ int main(int argc, char** argv){
 	}
 	outFile <<");"<<endl;
 
-	outFile <<"end "<<topComponent->name()<<";"<<endl<<endl;
-
-	outFile<<"architecture arch of "<<topComponent->name()<<" is"<<endl;
+	outFile <<"end "<<communicationHardwareComponent->componentInfo->name<<";"<<endl<<endl;
 
 	outFile <<"-- Component Declaration"<<endl<<endl;
-	set<HardwareComponent::HardwareComponentInfo*> usedComponentsInfo = getUsedComponents(topComponent);
-	/*generate the component declaration in the architecture file*/
-	for(set<HardwareComponent::HardwareComponentInfo*>::iterator it = usedComponentsInfo.begin(); it!= usedComponentsInfo.end(); it ++){
-		outFile<<(*it)->componentDeclaration<<endl;
 
-	}
+	outFile.close();
 
+}
+
+
+/*
+ * IP generation
+ * argv[1] : communication hardware xml description file
+ * argv[2] : output file location
+ */
+int main(int argc, char** argv){
+
+	char workingDir[256];
+
+
+	cout<<"parsing file"<<argv[1]<<endl;
+	CommunicationHardwareXmlParser * parser = new CommunicationHardwareXmlParser();
+	HardwareComponent* communicationHardwareComponent = parser->parseXml(string(argv[1]));
+
+	CommunicationHardwareConverterVHDL *converter = new CommunicationHardwareConverterVHDL();
+
+	cout<<"parsing to file"<<argv[2]<<endl;
+	converter->buildComponentDescription(string(argv[2]), communicationHardwareComponent);
+	
 }
