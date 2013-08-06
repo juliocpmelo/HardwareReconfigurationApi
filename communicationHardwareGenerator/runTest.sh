f96b6d04697ab6b9d9b30006b0eb4e27a84ddc43
Julio Melo <julio.engcomp@gmail.com>
2013-08-06 18:24:10 +0200

    Moving files from the deleted directory

---
+++ communicationHardwareGenerator/runTest.sh
@@ -0,0 +1,5 @@
+#o primeiro path corresponde à localização da lib libsystemc.so, a segunda não precisa mudar desde que execute esse
+#script no diretório <projeto>/processor
+export LD_LIBRARY_PATH=../lib:../systemc-2.3.0/lib-cygwin/:../iconv-1.9.2.win32/bin:../libxml2-2.7.8.win32/bin:../zlib-1.2.5/bin  
+export PATH=$PATH:$LD_LIBRARY_PATH
+./communicationHardwareGen $1 $2
