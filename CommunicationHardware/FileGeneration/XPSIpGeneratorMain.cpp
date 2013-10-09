ce96d2366b786607f94b799f1a57384364a04f5a
Julio Melo <julio.engcomp@gmail.com>
2013-10-09 18:43:30 +0200

    Fixing header generation to work with the new API

--- CommunicationHardware/FileGeneration/XPSIpGeneratorMain.cpp
+++ CommunicationHardware/FileGeneration/XPSIpGeneratorMain.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 #include <map>
 
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
 #include "CommunicationHardwareConverterVHDL.h"
 #include "CommunicationHardwareXmlParser.h"
 #include "HardwareProject.h"
@@ -13,7 +19,7 @@ void printUsage(){
 
 	cout<<"this program generates a set of componenent files to be used together with the HardwareReconfiguration API"<<endl;
 	cout<<"usage:"<<endl;
-	cout<<"./VhdlComponentGenerator <xmlProjectDescription>"<<endl;
+	cout<<"./CommHardwareComponentGenerator [vhdl|verilog]+ <xmlProjectDescription>"<<endl;
 	
 }
 /*
@@ -23,29 +29,47 @@ void printUsage(){
  */
 int main(int argc, char** argv){
 
-	char workingDir[256];
 
-	if (argc != 2){
+	if (argc != 3){
 		printUsage();
 		return -1;
 	}
 
-	cout<<"parsing file "<<argv[1]<<endl;
 
-
+/*TODO add verilog support*/
 //	HardwareProject *project= new HardwareProject("D:/AutonomicSegmentationSystem/AutonomicImageProcessingDesc.xml");
-	HardwareProject *project= new HardwareProject(string(argv[1]));
+//
+//
+	HardwareProject *project= new HardwareProject(string(argv[2]));
 
 	map<std::string, ReconfigurableRegion* >	communicationHardwareTable = project->managedReconfRegions;
 
-	CommunicationHardwareConverterVHDL *converter = new CommunicationHardwareConverterVHDL();
 
 
 
-	map<std::string, ReconfigurableRegion* >::iterator it;
-	for (it = communicationHardwareTable.begin(); it!=communicationHardwareTable.end(); it++){
-		cout<<"generating file "<<project->projectPath<< "/ipcores/"<<it->second->communicationHardware->name()<< ".vhdl"<<endl;
-		converter->buildComponentDescription(project->projectPath, it->second->communicationHardware);
+	if(strcmp(argv[1], "vhdl") == 0){
+		
+		cout<<"generating component files for vhdl"<<endl;
+		CommunicationHardwareConverterVHDL *converter = new CommunicationHardwareConverterVHDL();
+		map<std::string, ReconfigurableRegion* >::iterator it;
+		
+		cout<<"generating vhdl to component "<<endl;
+		if (mkdir (string(project->projectPath + "/commHardwareComponents").c_str(), 0755) == -1) { 
+			if (errno != EEXIST){
+				cout <<"Failed to create directory " << project->projectPath + "/commHardwareComponents" << endl;
+				return -1;
+			}
+		}
+
+		for (it = communicationHardwareTable.begin(); it!=communicationHardwareTable.end(); it++){
+			cout<<"generating file "<<project->projectPath<< "/ipcores/"<<it->second->communicationHardware->name()<< ".vhdl"<<endl;
+
+			string fileLocation = string(project->projectPath + "/commHardwareComponents/" + it->second->communicationHardware->name() + ".vhdl");
+			converter->buildComponentDescription(fileLocation, it->second->communicationHardware);
+		}
+	}
+	else{
+		cout<<"language "<<argv[2]<<" not supported yet"<<endl;
 	}
 
 	return 0;
