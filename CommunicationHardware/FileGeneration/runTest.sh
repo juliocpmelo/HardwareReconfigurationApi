9ee278097146d549a9841ffba686b0392a857355
Julio Melo <julio.engcomp@gmail.com>
2013-10-09 18:44:24 +0200

    Adding new argument for execution

--- CommunicationHardware/FileGeneration/runTest.sh
+++ CommunicationHardware/FileGeneration/runTest.sh
@@ -2,4 +2,4 @@
 #script no diretório <projeto>/processor
 export LD_LIBRARY_PATH=../../lib:../../systemc-2.3.0/lib-cygwin/:../../iconv-1.9.2.win32/bin:../../libxml2-2.7.8.win32/bin:../../zlib-1.2.5/bin  
 export PATH=$PATH:$LD_LIBRARY_PATH
-./bin/communicationHardwareFileGen $1 2>1
+./bin/communicationHardwareFileGen $1 $2
