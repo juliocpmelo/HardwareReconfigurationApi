d4e7819c66d831142b664f2f5011ecc74add38aa
Julio Melo <julio.engcomp@gmail.com>
2013-10-15 18:22:11 +0200

    Debug script to easy the process of running with gdb

---
+++ tests/runTest_debug.sh
@@ -0,0 +1,9 @@
+#o primeiro path corresponde à localização da lib libsystemc.so, a segunda não precisa mudar desde que execute esse
+#script no diretório <projeto>/processor
+#hand provided
+export XILINX_ROOT_DIRECTORY=/cygdrive/c/Xilinx/14.4/ISE_DS/
+#hand provided
+export XILINX_XFLOW_PATH=$XILINX_ROOT_DIRECTORY/ISE/bin/nt64/
+export LD_LIBRARY_PATH=../lib:../systemc-2.3.0/lib-cygwin/:../iconv-1.9.2.win32/bin:../libxml2-2.7.8.win32/bin:../zlib-1.2.5/bin  
+export PATH=$PATH:$LD_LIBRARY_PATH
+gdb ./bin/HardwareReconfTests
