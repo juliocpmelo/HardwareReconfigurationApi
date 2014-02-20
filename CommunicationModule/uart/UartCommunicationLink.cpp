935213534b36ab1af91cee8c8c7c27f4cc3cb5eb
Julio Melo <julio.engcomp@gmail.com>
2014-02-20 17:16:34 +0100

    Adding compilable version of the uart communicationg

--- CommunicationModule/uart/UartCommunicationLink.cpp
+++ CommunicationModule/uart/UartCommunicationLink.cpp
@@ -1,24 +1,88 @@
+#include "UartCommunicationLink.h"
 
-#ifndef UartCommunicatioinLink_h
-#define UartCommunicatioinLink_h
-#include "CommunicationLink.h"
+#include <stdio.h>   // standard input / output functions
+#include <fcntl.h>   // File control definitions
+#include <unistd.h>
 
-#include <termios.h>
 
-class UartCommunicationLink : public CommunicationLink{
-	public:
-		int uartPortFd;
+using namespace std;
 
-	public:
-		UartCommunicationLink(char* uartPort, speed_t portSpeed);
+CommunicationLink* CommunicationLink::createCommunicationLink(){
 
-		void configureUartPortSpeed(speed_t portSpeed);
-		void setPortAttr(int attrToSet, bool value);	
+	struct termios portSettings;
+	cfsetispeed(&portSettings, B115200);    // set baud rates
+	cfsetospeed(&portSettings, B115200);
 
-		/*inherit from CommunicationLink*/	
-		virtual void implSendMessage (uint8_t * buffer, int bufferSize);
-		virtual void implGetMessage (uint8_t * buffer, int messageSize);
+	portSettings.c_cflag |= PARENB;   // set parity(odd), stop bits, data bits
+	portSettings.c_cflag |= PARODD;
+	portSettings.c_cflag &= ~CSTOPB;
+	portSettings.c_cflag &= ~CSIZE;
+	portSettings.c_cflag |= CS8;
+	//port_settings.c_cflag |= (CLOCAL | CREAD);
+	portSettings.c_lflag |= (ICANON | ECHO | ECHOE);
 
-};
 
-#endif// UartCommunicatioinLink_h
+	return (CommunicationLink*) new UartCommunicationLink(string("/dev/ttyS0"), portSettings);
+}
+
+UartCommunicationLink::UartCommunicationLink(std::string uartPort,  struct termios portSettings){
+
+
+	uartPortFd = open("/dev/ttyS0", O_RDWR );
+
+	if(uartPortFd == -1) // if open is unsucessful
+	{
+		perror("open_port: Unable to open /dev/ttyS0 - ");
+	}
+
+	configureUartPort(portSettings);
+
+
+}
+
+void UartCommunicationLink::configureUartPort(struct termios portSettings)
+{
+	tcsetattr(uartPortFd, TCSANOW, &portSettings);
+}
+
+/*sets the binary values of the port configuration structure*/
+void UartCommunicationLink::setPortAttr(int attrToSet, bool value){
+
+	struct termios port_settings;      // structure to store the port settings in
+	tcgetattr(uartPortFd, &port_settings);
+
+	if (value)
+		port_settings.c_cflag |= attrToSet;
+	else
+		port_settings.c_cflag &= ~attrToSet;
+	
+	tcsetattr(uartPortFd, TCSANOW, &port_settings);    // apply the settings to the port
+}
+
+
+void UartCommunicationLink::implSendMessage (uint8_t * buffer, int bufferSize){
+	int loopNumber = bufferSize  / 4 + (bufferSize % 4 != 0 ? 1 : 0);
+	int bytesSent = 0;
+
+	int bytesToWrite = 4;
+
+	for(int i=0; i<loopNumber ; i++){
+		if(loopNumber + 1 == loopNumber)
+			bytesToWrite = bufferSize % 4 ;
+		bytesSent += write(uartPortFd, buffer + bytesSent, bytesToWrite);
+		usleep(300000); //pc was too fast to uart communication
+	}
+
+
+}
+
+
+void UartCommunicationLink::implGetMessage (uint8_t * buffer, int messageSize){
+
+	int bytesRead  = 0;
+	while(bytesRead < messageSize)
+		bytesRead += read(uartPortFd, buffer + bytesRead, messageSize - bytesRead);
+
+}
+
+
--- CommunicationModule/uart/UartCommunicationLink.h
+++ CommunicationModule/uart/UartCommunicationLink.h
@@ -1,95 +1,27 @@
 
+#ifndef UartCommunicatioinLink_h
+#define UartCommunicatioinLink_h
+#include "CommunicationLink.h"
 
-UartCommunicationLink::UartCommunicationLink(char* uartPort, speed_t portSpeed){
+#include <termios.h>
 
-
-	uartPortFd = open("/dev/ttyS0", O_RDWR );
-
-	if(uartPortFd == -1) // if open is unsucessful
-	{
-		perror("open_port: Unable to open /dev/ttyS0 - ");
-	}
-
-	configureUartPort(portSettings);
+#include <iostream>
 
 
-}
+class UartCommunicationLink : public CommunicationLink{
+	public:
+		int uartPortFd;
 
-void UartCommunicationLink::configureUartPortSpeed(speed_t portSpeed)
-{
+	public:
+		UartCommunicationLink(std::string uartPort, struct termios portSettings);
 
-	cfsetispeed(&port_settings, portSpeed);    // set baud rates
-	cfsetospeed(&port_settings, portSpeed);
+		void configureUartPort(struct termios portSettings);
+		void setPortAttr(int attrToSet, bool value);	
 
-	port_settings.c_cflag |= 
+		/*inherit from CommunicationLink*/	
+		virtual void implSendMessage (uint8_t * buffer, int bufferSize);
+		virtual void implGetMessage (uint8_t * buffer, int messageSize);
 
-}
+};
 
-/*sets the binary values of the port configuration structure*/
-void UartCommunicationLink::setPortAttr(int attrToSet, bool value){
-
-	struct termios port_settings;      // structure to store the port settings in
-	tcgetattr(uartPort, &port_settings);
-
-	if (value)
-		port_settings |= attrToSet;
-	else
-		port_settings &= ~attrToSet;
-	
-	tcsetattr(uartPort, TCSANOW, &port_settings);    // apply the settings to the port
-}
-
-
-void UartCommunicationLink::implSendMessage (uint8_t * buffer, int bufferSize){
-	int loopNumber = bufferSize  / 4 + (bufferSize % 4 != 0 ? 1 : 0);
-	int bytesSent = 0;
-
-	int bytesToWrite = 4;
-
-	for(int i=0; i<loopNumber ; i++){
-		if(loopNumber + 1 == loopNumber)
-			bytesToWrite = bufferSize % 4 ;
-		bytesSent += write(serialInterface, buf + bytesSent, bytesToWrite);
-		usleep(300000); //pc was too fast to uart communication
-	}
-
-
-}
-
-
-void UartCommunicationLink::implGetMessage (uint8_t * buffer, int messageSize){
-
-	int bytesRead  = 0;
-	while(bytesRead < messageSize)
-		bytesRead += read(serialInterface, buffer + bytesRead, messageSize - bytesRead);
-
-}
-
-
-/*implementation required by CommunicationLink*/	
-CommunicationLink* CommunicationLink::createCommunicationLink(){
-
-	struct termios port_settings;
-
-	UartCommunicationLink *uartCommLink =new UartCommunicationLink("/dev/ttyS0", B115200);
-
-	uartCommLink->setPortAttr(PARENB, true);
-	uartCommLink->setPortAttr(PARODD, true);
-	uartCommLink->setPortAttr(CSTOPB, false);
-	uartCommLink->setPortAttr(CSIZE, false);
-	uartCommLink->setPortAttr(CS8, true);
-	uartCommLink->setPortAttr(ICANON, true);
-	uartCommLink->setPortAttr(ECHO, true);
-	uartCommLink->setPortAttr(ECHOE, true);
-
-/*	port_settings.c_cflag |= PARENB;   // set parity(odd), stop bits, data bits
-	port_settings.c_cflag |= PARODD;
-	port_settings.c_cflag &= ~CSTOPB;
-	port_settings.c_cflag &= ~CSIZE;
-	port_settings.c_cflag |= CS8;
-	//port_settings.c_cflag |= (CLOCAL | CREAD);
-	port_settings.c_lflag |= (ICANON | ECHO | ECHOE);
-*/
-	return uartCommLink;
-
-}
+#endif// UartCommunicatioinLink_h
